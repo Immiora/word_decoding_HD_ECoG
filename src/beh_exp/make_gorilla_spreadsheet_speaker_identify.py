1d212676bba5667b8270d55c921743206f909614
immiora <immiora@gmail.com>
2021-12-02 14:58:31 +0100

    only opt for 1&1, relax alt constraints

--- src/beh_exp/make_gorilla_spreadsheet_speaker_identify.py
+++ src/beh_exp/make_gorilla_spreadsheet_speaker_identify.py
@@ -5,6 +5,9 @@ python beh_exp/make_gorilla_spreadsheet_speaker_identify.py \
     --path_csv /Fridge/users/julia/project_decoding_jip_janneke/data/beh_exp/spreadsheet_task2.csv \
     --path_stim /Fridge/users/julia/project_decoding_jip_janneke/results/optuna/jip_janneke/beh_stimuli
 
+Changes 2/12/2021:
+    - only opt-true
+    - relax constraint on same-syllable for alternative answer option
 '''
 
 import argparse
@@ -34,7 +37,8 @@ def main(args):
     wavs = [op.basename(i) for i in glob.glob(op.join(args.path_stim, '*.wav'))]
 
     # take only names of reconstructions, target names are the same
-    wavs_recon = [i for i in wavs if '_recon' in i]
+    # wavs_recon = [i for i in wavs if '_recon' in i]
+    wavs_recon = [i for i in wavs if 'opt-true_recon' in i]
 
     # randomize order of stimuli
     random.shuffle(wavs_recon)
@@ -45,8 +49,11 @@ def main(args):
     # take random subject of same sex
     sub = lambda x: int(x.split('sub-')[1].split('_')[0])
     rep = lambda x, y: x.replace('sub-' + x.split('sub-')[1].split('_')[0], 'sub-' + str(y))
-    res2_ = [rep(i, d[d['sex']==d[d['id']==sub(i)]['sex'].values[0]]['id'].drop(d[d['id'] == sub(i)].index).sample().values[0])
-                                                                                                        for i in ans]
+    # res2_ = [rep(i, d[d['sex']==d[d['id']==sub(i)]['sex'].values[0]]['id'].drop(d[d['id'] == sub(i)].index).sample().values[0])
+    #                                                                                                    for i in ans]
+    res2_ = [rep(i, d.drop(d[d['id'] == sub(i)].index).sample()['id'].values[0]) for i in ans]
+
+
     # randomize place of correct response between res1 and res2 columns
     res1, res2 = zip(*[random.sample(sublist, 2) for sublist in zip(ans, res2_)])
 
--- src/beh_exp/make_gorilla_spreadsheet_word_identify.py
+++ src/beh_exp/make_gorilla_spreadsheet_word_identify.py
@@ -5,6 +5,9 @@ python beh_exp/make_gorilla_spreadsheet_word_identify.py \
     --path_csv /Fridge/users/julia/project_decoding_jip_janneke/data/beh_exp/spreadsheet_task1.csv \
     --path_stim /Fridge/users/julia/project_decoding_jip_janneke/results/optuna/jip_janneke/beh_stimuli
 
+Changes 2/12/2021:
+    - only opt-true
+    - relax constraint on same-syllable for alternative answer option
 '''
 
 import argparse
@@ -38,7 +41,8 @@ def main(args):
     wavs = [op.basename(i) for i in glob.glob(op.join(args.path_stim, '*.wav'))]
 
     # take only names of reconstructions, target names are the same
-    wavs_recon = [i for i in wavs if '_recon' in i]
+    # wavs_recon = [i for i in wavs if '_recon' in i]
+    wavs_recon = [i for i in wavs if 'opt-true_recon' in i]
 
     # randomize order of stimuli
     random.shuffle(wavs_recon)
@@ -47,8 +51,10 @@ def main(args):
     ans = [i.split('word-')[1].split('_')[0] for i in wavs_recon]
 
     # take random word of same number of syllables as second option
-    res2_ = [d[d['syl']==d[d['word']==i]['syl'].values[0]]['word'].drop(d[d['word'] == i].index).sample().values[0]
-                                                                                                        for i in ans]
+    # res2_ = [d[d['syl']==d[d['word']==i]['syl'].values[0]]['word'].drop(d[d['word'] == i].index).sample().values[0]
+    #                                                                                                     for i in ans]
+    res2_ = [d.drop(d[d['word'] == i].index).sample()['word'].values[0] for i in ans]
+
     # randomize place of correct word between res1 and res2 columns
     res1, res2 = zip(*[random.sample(sublist, 2) for sublist in zip(ans, res2_)])
 
